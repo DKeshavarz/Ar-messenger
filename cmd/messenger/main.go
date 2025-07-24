7e357ee5243685fa933afa863f076c9e25b7e79e
DKeshavarz <dankeshavarz1075@gmail.com>
2025-07-24 14:06:29 +0330

    Chore: use cofig at main

--- cmd/messenger/main.go
+++ cmd/messenger/main.go
@@ -4,13 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/DKeshavarz/Ar-messenger/internal/config"
 	"github.com/DKeshavarz/Ar-messenger/internal/handlers"
 	"github.com/DKeshavarz/Ar-messenger/internal/services"
 	"github.com/gorilla/mux"
 )
 
 func main(){
-	
 	svc := services.NewRoomService(nil) // Replace nil with an actual MessageRepository implementation
     handler := handlers.NewWebSocketHandler(svc)
 
@@ -18,6 +18,6 @@ func main(){
     router.HandleFunc("/{chatName}/username", handler.HandleWebSocket)
 
 
-    log.Println("Server starting on :8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Println("Server starting on :" + config.GetEnvValue("SERVER_PORT"))
+    log.Fatal(http.ListenAndServe(":" + config.GetEnvValue("SERVER_PORT") , nil))
 }
