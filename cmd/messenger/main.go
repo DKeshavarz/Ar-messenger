0340462922a5b30d21deb74e44b8999ecf77d0fe
DKeshavarz <dankeshavarz1075@gmail.com>
2025-07-24 13:43:37 +0330

    Chore: change main.go to have entrypoint

--- cmd/messenger/main.go
+++ cmd/messenger/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/DKeshavarz/Ar-messenger/internal/handlers"
+	"github.com/DKeshavarz/Ar-messenger/internal/services"
+	"github.com/gorilla/mux"
+)
+
 func main(){
 	
+	svc := services.NewRoomService(nil) // Replace nil with an actual MessageRepository implementation
+    handler := handlers.NewWebSocketHandler(svc)
+
+    router := mux.NewRouter()
+    router.HandleFunc("/{chatName}/username", handler.HandleWebSocket)
+
+
+    log.Println("Server starting on :8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
