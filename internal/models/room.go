6f6e8aa64badefc17368b55908ed8878464d6f26
DKeshavarz <dankeshavarz1075@gmail.com>
2025-07-24 12:40:59 +0330

    Chore: Improve room models

--- internal/models/room.go
+++ internal/models/room.go
@@ -1,10 +1,42 @@
 package models
 
+import "sync"
+
 type Room struct {
 	RoomName  string
 	Clients   map[*Client]bool
 	Broadcast chan Message
 	Join      chan *Client
 	Leave     chan *Client
-	//mu        sync.RWMutex -> TODO: implement mutex for thread safety
+	mu        sync.RWMutex 
+}
+
+func (r *Room) Run() {
+    for {
+        select {
+        case client := <-r.Join:
+            r.mu.Lock()
+            r.Clients[client] = true
+            r.mu.Unlock()
+
+        case client := <-r.Leave:
+            r.mu.Lock()
+            delete(r.Clients, client)
+            r.mu.Unlock()
+            client.Conn.Close()
+			
+        case msg := <-r.Broadcast:
+            r.mu.RLock()
+            for client := range r.Clients {
+                if client.RoomName == msg.RoomName {
+                    if err := client.Conn.WriteJSON(msg); err != nil {
+                        r.mu.RUnlock()
+                        r.Leave <- client
+                        r.mu.RLock()
+                    }
+                }
+            }
+            r.mu.RUnlock()
+        }
+    }
 }
